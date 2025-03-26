6628833799e537b32c459faf3d329e2b530ef6ba
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-26 11:57:00 +0100

    Implement tab navigation with buttons for HTML, CSS, JS, and PHP

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
 import { useState } from 'react'
 
+const tabs = [
+    {
+        id: 1,
+        name: "HTML",
+        content: "HTML Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique neque aliquid labore voluptatibus laboriosam aliquam minima officiis tempora asperiores alias accusantium veritatis eius molestiae mollitia suscipit odit illum, velit ducimus!"
+    },
+    {
+        id: 2,
+        name: "CSS",
+        content: "CSS Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique neque aliquid labore voluptatibus laboriosam aliquam minima officiis tempora asperiores alias accusantium veritatis eius molestiae mollitia suscipit odit illum, velit ducimus!"
+    },
+    {
+        id: 3,
+        name: "JS",
+        content: "JS Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique neque aliquid labore voluptatibus laboriosam aliquam minima officiis tempora asperiores alias accusantium veritatis eius molestiae mollitia suscipit odit illum, velit ducimus!"
+    },
+    {
+        id: 4,
+        name: "PHP",
+        content: "PHP Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique neque aliquid labore voluptatibus laboriosam aliquam minima officiis tempora asperiores alias accusantium veritatis eius molestiae mollitia suscipit odit illum, velit ducimus!"
+    }
+]
+
 export default function App() {
+
+    function handleTabClick() {
+        console.log('Cliccato');
+
+    }
 
     return (
 
         <>
-            <h1>Ciao</h1>
+            <h1>Tabs</h1>
+
+            <div className="container">
+                <div>
+                    {
+                        tabs.map(tab => (
+                            <button key={`tab-${tab.id}`} className="btn btn-primary" onClick=""}
+                    ))
+                    }
+
+                </div>
+
+
+                <div className="content">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore magnam architecto veritatis, aliquid autem eveniet quibusdam quas assumenda! Perspiciatis fugiat ipsam similique pariatur, ipsum esse! Doloribus architecto beatae quis similique!
+                </div>
+
+
+            </div>
         </>
 
     )
 }
+
+<button className='btn btn-primary me-2'>HTML</button>
+                <button className='btn btn-primary me-2'>CSS</button>
+                <button className='btn btn-primary me-2'>JS</button>
+                <button className='btn btn-primary me-2'>PHP</button>
